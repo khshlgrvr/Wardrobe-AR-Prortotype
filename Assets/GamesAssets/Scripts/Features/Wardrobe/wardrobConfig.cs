f85ba9455fe97c0b39e8f48345029f081acebab2
khushalGrover <khushal15grover@gmail.com>
2026-03-11 00:53:33 +0530

    base cabinet demension

---
+++ Assets/GamesAssets/Scripts/Features/Wardrobe/wardrobConfig.cs
@@ -0,0 +1,91 @@
+using UnityEngine;
+
+[ExecuteAlways]
+public class WardrobeConfig : MonoBehaviour
+{
+    [Header("Dimensions (mm)")]
+    public float width = 1200f;
+    public float height = 2000f;
+    public float depth = 600f;
+    public float thickness = 18f;
+
+    [Header("Panels")]
+    public Transform leftPanel;
+    public Transform rightPanel;
+    public Transform topPanel;
+    public Transform bottomPanel;
+    public Transform backPanel;
+    public Transform frontLeftPanel;
+    public Transform frontRightPanel;
+    public Transform leftHinges;
+    public Transform rightHinges;
+
+    float W;
+    float H;
+    float D;
+    float T;
+
+    void Update()
+    {
+        BuildWardrobe();
+    }
+
+    void BuildWardrobe()
+    {
+        if (leftPanel == null || rightPanel == null || topPanel == null || bottomPanel == null || backPanel == null)
+            return;
+
+        // convert mm to meters
+        W = width / 1000f;
+        H = height / 1000f;
+        D = depth / 1000f;
+        T = thickness / 1000f;
+
+        // LEFT PANEL
+        leftPanel.localPosition = new Vector3(0, 0, 0);
+        leftPanel.localScale = new Vector3(T, H, D);
+
+        // RIGHT PANEL
+        rightPanel.localPosition = new Vector3(-W + T, 0, 0);
+        rightPanel.localScale = new Vector3(T, H, D);
+
+        // BOTTOM PANEL
+        bottomPanel.localPosition = new Vector3(-T, 0, 0);
+        bottomPanel.localScale = new Vector3(W - (2 * T), T, D);
+
+        // TOP PANEL
+        topPanel.localPosition = new Vector3(-T, H - T, 0);
+        topPanel.localScale = new Vector3(W - (2 * T), T, D);
+
+        // BACK PANEL
+        backPanel.localPosition = new Vector3(-T, T, 0);
+        backPanel.localScale = new Vector3(W-(2*T), H-(2*T), T);
+
+
+        // Hinges
+        if (leftHinges != null)
+        {
+            leftHinges.localPosition = new Vector3(0, H / 2, D);
+        }
+        if (rightHinges != null)
+        {
+            rightHinges.localPosition = new Vector3(-W , H / 2, D);
+        }
+
+        //FrontLeftPanel pivot on left Hinges
+        if (frontLeftPanel != null)
+        {
+            frontLeftPanel.localPosition = new Vector3(T, 0, 0);
+            frontLeftPanel.localScale = new Vector3(T, H, W/2);
+            frontLeftPanel.SetParent(leftHinges, true);
+        }
+        //FrontRightPanel pivot on right Hinges
+        if (frontRightPanel != null)
+        {
+            frontRightPanel.localPosition = new Vector3(0, 0, 0);
+            frontRightPanel.localScale = new Vector3(T, H, W/2);
+            frontRightPanel.SetParent(rightHinges, true);
+        }
+        
+    }
+}
